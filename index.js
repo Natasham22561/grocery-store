e92ba38df5fc1991aca50423d62072e8ede5f541
Natasha Mckenzie <natashamckenzie561@yahoo.com>
2025-08-05 21:37:10 -0400

    made changes

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ const inventory = [
  */
 function logNames(items) {
   // TODO: use `forEach`
+  items.forEach(item => { 
+    console.log(item.name);
+});
 }
 
 /**
@@ -35,33 +38,38 @@ function logNames(items) {
  */
 function getUppercaseNames(items) {
   // TODO: use `map`
+  return items.map(item => item.name.toUpperCase());
 }
 
 /**
- * @param {Item[]} items - array of items
- * @param {number} id - id of the item to find
+@@ -43,7 +45,7 @@
  * @returns {Item} - the item in `items` with the given `id`
  */
 function getItemById(items, id) {
   // TODO: use `find`
+  return items.find(item => item.id === id);
 }
 
 /**
- * @param {Item[]} items - array of items
- * @param {string} name - name of the item to find
+@@ -52,7 +54,11 @@
  * @returns {number} the price of the item named `name` if found
  */
 function getItemPriceByName(items, name) {
   // TODO: use a loop!
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].name === name) {
+      return items[i].price;
+    }
+  }
 }
 
 /**
- * @param {Item[]} items - array of items
- * @param {string} category
+@@ -61,23 +67,25 @@
  * @returns {Item[]} array of items that belong to the given `category`
  */
 function getItemsByCategory(items, category) {
   // TODO: use `filter`
+  return items.filter(item => item.category === category);
 }
 
 /**
@@ -70,6 +78,8 @@ function getItemsByCategory(items, category) {
  */
 function countItems(items) {
   // TODO: use `reduce`
+  const totalCount = items.reduce((total, item) => total + item.quantity, 0);
+console.log(totalCount);
 }
 
 /**
@@ -78,32 +88,8 @@ function countItems(items) {
  */
 function getTotalPrice(items) {
   // TODO: use `reduce`
+  const priceTotal = items.reduce((total, item) => total + item.price, 0);
+console.log(priceTotal);
 }
 
-// === READ BUT DO NOT CHANGE THE CODE BELOW ===
-
-console.log("Welcome! We carry the following items:");
-logNames(inventory);
-
-console.log("Here are the names again in all uppercase:");
-console.log(getUppercaseNames(inventory));
-
-console.log(`In total, we have ${countItems(inventory)} items in stock.`);
-
-const totalCost = getTotalPrice(inventory);
-console.log(
-  `It would cost $${totalCost?.toFixed(2)} to purchase everything in stock.`
-);
-
-const itemId = prompt("Enter the ID of an item:", "1");
-console.log(`The item with id #${itemId} is:`);
-console.log(getItemById(inventory, +itemId));
-
-const itemName = prompt("Enter the name of an item:", "apple");
-console.log(
-  `The price of ${itemName} is ${getItemPriceByName(inventory, itemName)}.`
-);
-
-const category = prompt("Enter a category you would like to see:", "fruit");
-console.log(`The items in the ${category} category are:`);
-console.log(getItemsByCategory(inventory, category));
+// === READ BUT DO NOT CHANGE THE CODE BELOW===
